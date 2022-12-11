d5eaf196f6ecedd184a2ead63afa146566ec9345
Johannes Kasimir <johannes.kasimir@math.lu.se>
2022-12-11 21:19:36 +0100

    feat: add details view

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ function dice_throws(n) {
     return r.sort().reverse();
 }
 
-function fight(defenders, attackers, min_attackers) {
+function fight(defenders, attackers, min_attackers, history) {
+    if (!history)
+        history = [{attackers, defenders}];
     if (!min_attackers)
         min_attackers = 1;
     if (attackers < min_attackers || defenders < 1)
-        return [defenders, attackers];
+        return [defenders, attackers, history];
 
     const d = dice_throws(Math.min(defenders, 2));
     const a = dice_throws(Math.min(attackers, 3));
+    history.push({attackers_dice: a, defenders_dice: d});
     for (let i=0;i<Math.min(d.length, a.length);i++) {
         if (d[i] >= a[i])
              attackers -= 1;
         else defenders -= 1;
+        history[history.length - 1].attackers = attackers;
+        history[history.length - 1].defenders = defenders;
         if (DEBUG) console.log({
             attack_dices: a,
             defend_dices: d,
@@ -32,7 +37,7 @@ function fight(defenders, attackers, min_attackers) {
             i: i
         });
     }
-    return fight(defenders, attackers, min_attackers);
+    return fight(defenders, attackers, min_attackers, history);
 }
 
 
@@ -55,12 +60,15 @@ function go() {
     const _attackers = parseInt(attackers_input.value);
     const min_attackers = parseInt(min_attackers_input.value);
 
-    const [defenders, attackers] = fight(_defenders, _attackers, min_attackers);
+    const [defenders, attackers, history] = fight(_defenders, _attackers, min_attackers);
+
+    const details = `<details><summary>Fight details</summary><pre>${JSON.stringify(history, null, 2)}</pre></details>`;
     if (defenders === 0) {
         log(
             SWORD, " Attackers win!<br>",
             "Move ", attackers, " troops to the territory.",
             " ( <span style=\"color: red;\">", attackers - _attackers, "</span> )",
+            details,
         );
         attackers_input.value = attackers - 1;
         defenders_input.value = 1;
@@ -73,6 +81,7 @@ function go() {
             " ( <span style=\"color: red;\">", defenders - _defenders, "</span> )",
             "<br>Remaining attackers: ", attackers, ".",
             " ( <span style=\"color: red;\">", attackers - _attackers, "</span> )",
+            details,
         );
         attackers_input.value = attackers;
         defenders_input.value = defenders;
